4c853893e4d0aafb6a0195cd15188c999c648845
manish <manishkumar@xebia.com>
2019-07-02 11:49:18 +0400

    Updates

---
+++ precache-manifest.6296948eb38310c94fdf01612d417853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3f0de037e76d32531d0928d7d2e146",
+    "url": "/trending-news/index.html"
+  },
+  {
+    "revision": "da2383f7a3601410dfa5",
+    "url": "/trending-news/static/css/main.4d55034a.chunk.css"
+  },
+  {
+    "revision": "5c0626166fe7963e493e",
+    "url": "/trending-news/static/js/2.15b59648.chunk.js"
+  },
+  {
+    "revision": "da2383f7a3601410dfa5",
+    "url": "/trending-news/static/js/main.0621fe07.chunk.js"
+  },
+  {
+    "revision": "00fe2aefe9524c07528c",
+    "url": "/trending-news/static/js/runtime~main.6c1291f7.js"
+  }
+]);
